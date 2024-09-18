ca1ee0df21c419b5f255152d332b862d1a4b34c7
Renato Andrade <renatoandrade.com@gmail.com>
2024-09-17 21:09:14 -0300

    feat: add db config

---
+++ src/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=mysql_yii2;dbname=yii2_database',
+    'username' => 'yii2_user',
+    'password' => 'yii2_password',
+    'charset' => 'utf8',
+];
